b5d1d4fe04828d17d0e53e0f9335bebd1740dcd4
Stefan Jenkner <stefan@jenkner.org>
2014-08-02 00:40:33 +0200

    Added serverspec tests.

---
+++ spec/kaspar/puppetmaster_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe package('apache2') do
+  it { should be_installed }
+end
+
+describe service('apache2') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe port(8140) do
+  it { should be_listening }
+end
